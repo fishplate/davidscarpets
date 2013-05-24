6ac48f71f5a051cd16ea733e9c5fb19514e110ae
richard <richard@richard-Inspiron-5523.(none)>
2013-05-24 23:13:53 +0100

    fixed karls shit

--- public/assets/js/valid_mail.js
+++ public/assets/js/valid_mail.js
@@ -1,22 +1,38 @@
-// var $mjq for better adaptation and work with other libraries
-var $mjq = jQuery.noConflict();
-$mjq(function(){
-  $mjq(document).ready(function(){
-     $mjq(".help-inline").each(function() {
-      $mjq(this).css('display', 'none');
+// var $ for better adaptation and work with other libraries
+
+$(function(){
+  $(document).ready(function(){
+     $(".help-inline").each(function() {
+      $(this).css('display', 'none');
      });
   });
-  $mjq("#inputEmail").bind('blur', is_valid_email);
-  $mjq("#inputName").bind('blur', is_valid_name);
-  $mjq("#inputSubject").bind('blur', is_valid_subject);
-  $mjq("#textarea").bind('blur', is_valid_comment);
-  $mjq('#validForm').bind('submit', function() {
+  $("#inputEmail").bind('blur', is_valid_email);
+  $("#inputTel").bind('blur', is_valid_tel);
+  $("#inputName").bind('blur', is_valid_name);
+  $('#validForm').bind('submit', function() {
     return is_valid_form();
   });
 });
+
+//Tel Validate
+function is_valid_tel() {
+  $this = $("#inputTel");
+  var pattern = new RegExp(/^\d{11}$/);
+  if(pattern.test($this.val())){ // valid
+    if ($this.closest(".control-group").hasClass("error"))
+      $this.closest(".control-group").removeClass("error");
+    $this.siblings(".help-inline").css("display", "none");
+    return true;
+  } else { // error
+    if (!$this.closest(".control-group").hasClass("error"))
+      $this.closest(".control-group").addClass("error");
+    $this.siblings(".help-inline").css("display", "block");
+    return false;
+  }
+}
 // Email validate
 function is_valid_email() {
-  $this = $mjq("#inputEmail");
+  $this = $("#inputEmail");
   var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
   if(pattern.test($this.val())){ // valid
     if ($this.closest(".control-group").hasClass("error"))
@@ -32,7 +48,7 @@ function is_valid_email() {
 }
 // Name validate
 function is_valid_name() {
-  $this = $mjq("#inputName");
+  $this = $("#inputName");
   if($this.val().length>0){ // valid
     if ($this.closest(".control-group").hasClass("error"))
       $this.closest(".control-group").removeClass("error");
@@ -45,42 +61,12 @@ function is_valid_name() {
     return false;
   }
 }
-// Subject validate
-function is_valid_subject() {
-  $this = $mjq("#inputSubject");
-  if($this.val().length>0){ // valid
-    if ($this.closest(".control-group").hasClass("error"))
-      $this.closest(".control-group").removeClass("error");
-    $this.siblings(".help-inline").css("display", "none");
-    return true
-  } else { // error
-    if (!$this.closest(".control-group").hasClass("error"))
-      $this.closest(".control-group").addClass("error");
-    $this.siblings(".help-inline").css("display", "block");
-    return false;
-  }
-}
-// Comment validate
-function is_valid_comment() {
-  $this = $mjq("#textarea");
-  if($this.val().length>0){ // valid
-    if ($this.closest(".control-group").hasClass("error"))
-      $this.closest(".control-group").removeClass("error");
-    $this.siblings(".help-inline").css("display", "none");
-    return true
-  } else { // error
-    if (!$this.closest(".control-group").hasClass("error"))
-      $this.closest(".control-group").addClass("error");
-    $this.siblings(".help-inline").css("display", "block");
-    return false;
-  }
-}
+
 // Form validate
 function is_valid_form() {
   var ret = true;
+  if (!is_valid_tel()) var ret = false;
   if (!is_valid_name()) var ret = false;
   if (!is_valid_email()) var ret = false;
-  if (!is_valid_subject()) var ret = false;
-  if (!is_valid_comment()) var ret = false;
   return ret;
 }
