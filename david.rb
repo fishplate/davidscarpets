b0240015bd7dd18a80f6d3c350b61c21f9795ae3
richard <richard@richard-Inspiron-5523.(none)>
2013-05-24 23:44:52 +0100

    Pony mail setup

--- david.rb
+++ david.rb
@@ -1,7 +1,43 @@
+require 'bundler/setup'
 require "rubygems"
 require 'sinatra'
+require 'sinatra/flash'
+require 'pony'
+require './config/config.rb' if File.exists?('./config/config.rb')
+
 
 
 get '/' do
   erb :index
 end
+
+post '/' do
+  
+    from = params[:name]
+    subject = "#{params[:name]} has contacted you from David's Carpets Website"
+
+    body = "#{params[:email]}#{params[:tel]}"
+
+  Pony.mail(
+  :from => from,
+  :to => ENV["EMAIL_ADDRESS"],
+  :subject => subject,
+  :body => body,
+  :via => :smtp,
+  :via_options => {
+    :address              => 'smtp.gmail.com',
+    :port                 => '587',
+    :enable_starttls_auto => true,
+    :user_name            => ENV["USER_NAME"],
+    :password             => ENV["PASSWORD"],
+    :authentication       => :plain, 
+    :domain               => "localhost.localdomain" 
+})
+  flash[:notice] = "Thanks for your email. We will be in touch soon."
+  redirect '/success' 
+
+end
+
+get('/success') do
+     erb :index 
+  end
